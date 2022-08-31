1ae7e2830f6b863cca6c606b6c95aef9dc950901
david.luques <david.luques@globons.com>
2022-08-31 09:12:24 -0300

    ejercicio 3 terminado

---
+++ src/Ejercicio3/EjercicioTres.java
@@ -0,0 +1,13 @@
+package Ejercicio3;
+
+public class EjercicioTres {
+
+        public static void main(String[] args) {
+
+            String[] soycadena ={"uno","dos","tres"};
+            for(int i=0 ; i <soycadena.length;i++){
+                System.out.println(soycadena[i]);
+            }
+
+        }
+}
